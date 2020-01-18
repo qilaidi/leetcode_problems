7bf9bd289fd8be893072d774abfa4d481d5d9d7a
张倩 <zhangqian_cd@g7.com.cn>
2020-01-18 18:45:28 +0800

    547 Friend Circles-disjoint set

--- src/547FriendCircles.py
+++ src/547FriendCircles.py
@@ -2,26 +2,50 @@
 # Create by zq
 # Create on 2020/1/16
 
-class Solution:
+class Solution1:
+    """dfs"""
     def findCircleNum(self, M):
         res = 0
+        visited = [0] * len(M)
         for i in range(len(M)):
-            for j in range(len(M[0])):
-                if M[i][j] == 1:
-                    res += 1
-                    self.dfs(M, i, j)
+            if visited[i] == 0:
+                res += 1
+                self.dfs(M, visited, i)
         return res
 
-    def dfs(self, M, i, j):
-        if i < 0 or i == len(M) or j < 0 or j == len(M[0]):
-            return
-        if M[i][j] == 0:
-            return
-        elif M[i][j] == 1:
-            self.dfs(M, i + 1, j)
-            self.dfs(M, i - 1, j)
-            self.dfs(M, i, j - 1)
-            self.dfs(M, i, j + 1)
+    def dfs(self, M, visited, i):
+        for j in range(len(M[0])):
+            if visited[j] == 0 and M[i][j] == 1:
+                visited[j] = 1
+                self.dfs(M, visited, j)
+
+class Solution:
+    """disjoint set"""
+    def findCircleNum(self, M):
+        n = len(M)
+        p = [i for i in range(n)]
+        for i in range(n):
+            for j in range(i, n):
+                if M[i][j] == 1 :
+                    self._union(p, i, j)
+        return len(set([self._parent(p, i) for i in range(n)]))
+
+    def _parent(self, p, i):
+        root = i
+        while p[root] != root:
+            root = p[root]
+        while p[i] != i:
+            x = i; i = p[i]; p[x] = root
+        return root
+    def _union(self, p, i, j):
+        p1 = self._parent(p, i)
+        p2 = self._parent(p, j)
+        p[p1] = p2
+
+
+
+
+
 
 if __name__ == '__main__':
     M1 = [[1,1,0],
