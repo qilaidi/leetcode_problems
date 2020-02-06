4b71ca6622ac18e2374acd44f875f1c9f87abef3
张倩 <zhangqian_cd@g7.com.cn>
2020-02-06 17:57:40 +0800

    231PowerOfTwo.py

---
+++ src/231PowerOfTwo.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+# Create by zq
+# Create on 2020/2/6
+
+class Solution:
+    def isPowerOfTwo(self, n):
+        return (n != 0) and (n & (n -1) == 0)
+
+if __name__ == '__main__':
+    test = Solution()
+    print(test.isPowerOfTwo(1)) # True
+    print(test.isPowerOfTwo(16)) # True
+    print(test.isPowerOfTwo(218)) # False
