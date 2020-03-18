e4f382a6400881101f81e541fc9026846556c49b
Kernel <qilaidi@netease.com>
2020-03-18 21:20:17 +0800

    206ReverseLinkedList

--- leetcode/206ReverseLinkedList.py
+++ leetcode/206ReverseLinkedList.py
@@ -7,11 +7,44 @@ class ListNode:
         self.val = x
         self.next = None
 
+    @classmethod
+    def generate(self, a_list):
+        list_node = [ListNode(x) for x in a_list]
+        for i in range(len(list_node)):
+            if i == len(list_node) - 1:
+                list_node[i].next = None
+            else:
+                list_node[i].next = list_node[i+1]
+        return list_node[0]
+
+    @classmethod
+    def print_list_node(self, head):
+        res = []
+        while head:
+            res.append(head.val)
+            head = head.next
+        return res
+
 class Solution:
+    def reverseList_recursive(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        node = self.reverseList_recursive(head.next)
+        head.next.next = head
+        head.next = None
+        return node
+
     def reverseList(self, head: ListNode) -> ListNode:
-        def _reverse(node, prev = None):
-            if not node:
-                return prev
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
 
-
-        return _reverse(head)
+if __name__ == '__main__':
+    head = ListNode.generate([1, 2, 3, 4, 5])
+    test = Solution()
+    node = test.reverseList(head)
+    print(ListNode.print_list_node(node))
