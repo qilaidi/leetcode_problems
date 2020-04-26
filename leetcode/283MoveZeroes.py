f0d4b1a6bf2716fb9e22ac1e5ca24c5c7a6cbffa
张倩 <zhangqian_cd@g7.com.cn>
2020-04-26 18:27:53 +0800

    283MoveZeroes 2 刷

--- leetcode/283MoveZeroes.py
+++ leetcode/283MoveZeroes.py
@@ -7,7 +7,7 @@ class Solution1:
         nums[:] = [i for i in nums if i != 0]
         nums[:] + [0] * counts
 
-class Solution:
+class Solution2:
     def moveZeroes(self, nums):
         zero_indexs = []
         for i in range(len(nums)):
@@ -17,6 +17,18 @@ class Solution:
                 if zero_indexs:
                     nums[zero_indexs.pop(0)], nums[i] = nums[i], 0
                     zero_indexs.append(i)
+        return nums
+
+class Solution:
+    def moveZeroes(self, nums):
+        """执行用时 :32 ms, 在所有 Python3 提交中击败了98.38%的用户"""
+        zero_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[zero_index] = nums[i]
+                if zero_index != i:
+                    nums[i] = 0
+                zero_index += 1
         return nums
 
 if __name__ == '__main__':
