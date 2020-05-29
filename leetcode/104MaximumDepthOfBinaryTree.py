3225a62cb2db4715695f703af8df99a079f0f8a1
张倩 <zhangqian_cd@g7.com.cn>
2020-05-29 17:58:42 +0800

    104MaximumDepthOfBinaryTree

---
+++ leetcode/104MaximumDepthOfBinaryTree.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+# Create by zq
+# Create on 2020/5/29
+from leetcode_problems.leetcode.common_tools.tree_node import TreeNode
+
+
+class Solution:
+    def maxDepth1(self, root: TreeNode) -> int:
+        "DFS"
+        def helper(root, res, level):
+            return max(helper(root.left, max(res, level), level+1), helper(root.right, max(res, level), level+1)) if root else res
+        return helper(root, 0, 1)
+
+    def maxDepth2(self, root: TreeNode) -> int:
+        "BFS 不优雅"
+        res, stack = 0, [root] if root else []
+        while stack:
+            res += 1
+            current_nodes = []
+            for node in stack:
+                if node.left:
+                    current_nodes.append(node.left)
+                if node.right:
+                    current_nodes.append(node.right)
+            stack = current_nodes
+        return res
+
+    def maxDepth3(self, root: TreeNode) -> int:
+        "DFS 优雅de光头哥"
+        return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
+
+    def maxDepth(self, root: TreeNode) -> int:
+        "BFS 优雅"
+        max_depth, stack = 0, [(1, root)] if root else []
+        while stack:
+            depth, root = stack.pop()
+            if root:
+                max_depth = max(depth, max_depth)
+                stack.append((depth+1, root.left))
+                stack.append((depth+1, root.right))
+        return max_depth
+
+if __name__ == '__main__':
+    null = None
+    root = TreeNode.generate_tree([3,9,20,null,null,15,7])
+    test = Solution()
+    print(test.maxDepth(root))
