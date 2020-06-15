81b2e7aeb1a0923523dd7065870efb633894cdeb
张倩 <zhangqian_cd@g7.com.cn>
2020-06-15 18:49:04 +0800

    14LongestCommonPrefix 官方每日一刷 20200615 O(n) 使用zip 和 set

--- leetcode/14LongestCommonPrefix.py
+++ leetcode/14LongestCommonPrefix.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 # Create by zq
 # Create on 2020/2/17
+from typing import List
+
 
 class Solution:
     def longestCommonPrefix_1(self, strs):
@@ -23,7 +25,7 @@ class Solution:
                         res += standard
         return res
 
-    def longestCommonPrefix(self, strs):
+    def longestCommonPrefix2(self, strs):
         if not strs:
             return ""
         shortest = min(strs, key=len)
@@ -32,6 +34,24 @@ class Solution:
                 if other[i] != ch:
                     return shortest[:i]
         return shortest
+
+    def longestCommonPrefix3(self, strs):
+        if not strs: return ""
+        standards = min(strs, key=len)
+        for i, ch in enumerate(standards):
+            for other in strs:
+                if other[i] != ch:
+                    return standards[:i]
+        return standards
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                res += i[0]
+            else: break
+        return res
+
 
 if __name__ == '__main__':
     test = Solution()
