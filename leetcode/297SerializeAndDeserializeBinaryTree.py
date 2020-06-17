829a2e67fe2f2edffd17966cfe48dd33ef2f1989
张倩 <zhangqian_cd@g7.com.cn>
2020-06-17 10:54:54 +0800

    297SerializeAndDeserializeBinaryTree 官方每日一刷活动20200616

--- leetcode/297SerializeAndDeserializeBinaryTree.py
+++ leetcode/297SerializeAndDeserializeBinaryTree.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 # Create by zq
 # Create on 2020/6/2
+from collections import deque
+
 from leetcode_problems.leetcode.common_tools import tree_node
 
 
@@ -11,7 +13,7 @@ class TreeNode(object):
         self.right = None
 
 
-class Codec:
+class Codec1:
 
     def serialize(self, root):
         """Encodes a tree to a single string.
@@ -60,14 +62,76 @@ class Codec:
                 i += 1
         return data[0] if data else None
 
+# You may serialize the following tree:
+#
+#     1
+#    / \
+#   2   3
+#      / \
+#     4   5
+#
+# as "[1,2,3,null,null,4,5]"
+#
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        res, stack = [], deque([root])
+        while stack:
+            node = stack.popleft()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+            else:
+                res.append(None)
+        while res and res[-1] is None:
+            res.pop()
+        return str(res)
+
+
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        data = eval(data)
+        if not data or not isinstance(data, list): return None
+        data = [TreeNode(x) if x is not None else None for x in data]
+        data_length, node_index, cur = len(data), 0, 1
+        while cur < data_length:
+            if data[node_index]:
+                data[node_index].left = data[cur]
+                cur += 1
+                if cur < data_length:
+                    data[node_index].right = data[cur]
+                    cur += 1
+            node_index += 1
+        return data[0]
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+# leetcode submit region end(Prohibit modification and deletion)
+
 
 if __name__ == '__main__':
     null = None
     codec = Codec()
     # root = codec.deserialize("[1,2,3,null,null,4,5]")
     # root = codec.deserialize("[-1,0,1]")
+    root = codec.deserialize("[]")
     # root = codec.deserialize("[5,2,3,null,null,2,4,3,1]")
     # root = codec.deserialize("[10,9,11,8,null,null,12,7,null,null,13,6,null,null,14,5,null,null,15,4,null,null,16,3,null,null,17,2,null,null,18,1,null,null,19,0]")
-    root = tree_node.TreeNode.generate_tree([10,9,11,8,null,null,12,7,null,null,13,6,null,null,14,5,null,null,15,4,null,null,16,3,null,null,17,2,null,null,18,1,null,null,19,0])
-    # print((codec.serialize(root)))
-    print(tree_node.TreeNode.print_tree_preorder(root))
+    # root = tree_node.TreeNode.generate_tree([10,9,11,8,null,null,12,7,null,null,13,6,null,null,14,5,null,null,15,4,null,null,16,3,null,null,17,2,null,null,18,1,null,null,19,0])
+    print((codec.serialize(root)))
+    # print(tree_node.TreeNode.print_tree_preorder(root))
